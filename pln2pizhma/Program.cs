66ecae4b3864b1148df4170960f006bbb24694c1
Victor <Victor@DESKTOP-RP3P58F>
2016-12-18 20:28:44 +0800

    添加项目文件。

---
+++ pln2pizhma/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pln2pizhma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
